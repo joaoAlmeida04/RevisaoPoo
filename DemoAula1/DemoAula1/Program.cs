2866c5d8e596280ee99a70637449c769536e3ea2
Alme1da04 <joaoaansaloni@gmail.com>
2025-08-19 22:13:04 -0300

    publicando dois exercicios, questão 1 se 2

---
+++ DemoAula1/DemoAula1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DemoAula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma altura:");
+            int altura = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite uma largura:");
+            int largura = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um deslocamento:");
+            int deslocamento = int.Parse(Console.ReadLine());
+
+            //linha de cima
+            Console.Write(ImprimirLinhaVazia(deslocamento));
+            Console.WriteLine(ImprimirLinhaCheia(largura)); 
+
+
+            //linhas do meio
+            for (int i = 0; i < altura-2; i++)
+            {
+                Console.Write(ImprimirLinhaVazia(deslocamento));
+                Console.Write('X');
+                Console.Write(ImprimirLinhaVazia(largura - 2));
+                Console.WriteLine('X');
+            }
+
+            //linh de baixo
+            Console.Write(ImprimirLinhaVazia(deslocamento));
+            Console.Write(ImprimirLinhaCheia(largura));
+
+        }
+
+        static string ImprimirLinhaVazia(int deslocamento)
+        {
+            return new string(' ', deslocamento);
+        }
+            
+        static string ImprimirLinhaCheia(int largura)
+        {
+            return new string ('X', largura);
+        }
+    }
+}
---
+++ exercicoRevisao01/exercicoRevisao01/Program.cs
@@ -0,0 +1,158 @@
+﻿namespace exercicoRevisao01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao = 6;
+            int[] vetor = null;
+            do
+            {
+                Console.WriteLine("1 - Dividir dois números");
+                Console.WriteLine("2 – Ler 3 números e mostrar qual é o maior deles");
+                Console.WriteLine("3 – Ler e armazenar N números");
+                Console.WriteLine("4 – Mostrar a soma dos números armazenados");
+                Console.WriteLine("5 - Contar quantos dos números armazenados são pares e quantos são ímpares");
+                Console.WriteLine("0 - Sair");
+                Console.WriteLine("Opção:");
+                opcao = int.Parse(Console.ReadLine());
+
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Digite um número:");
+                        int numero1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite outro número:");
+                        int numero2 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"O resultado é igual a: {DividirDoisNumeros(numero1, numero2)}");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Digite um número:");
+                        int num1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite outro número:");
+                        int num2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite outro número:");
+                        int num3 = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"O maior número é: {InformaMaiorNumero(num1, num2, num3)}");
+
+                        break;
+
+                    case 3:
+                        vetor = ArmazenarNumeros();
+                        break;
+
+                    case 4:
+
+                        if(vetor != null)
+                        {
+                            Console.WriteLine($"A soma dos número é: {SomarNumerosArmazenados(vetor)}");
+                        }
+                        else
+                        {
+                            vetor = ArmazenarNumeros();
+                            Console.WriteLine($"A soma dos número é: {SomarNumerosArmazenados(vetor)}");
+                        }
+                            break;
+
+                    case 5:
+
+                        if (vetor != null)
+                        {
+                            int pares = 0;
+                            int impares = 0;
+
+                            for (int i = 0; i < vetor.Length; i++)
+                            {
+                                if (vetor[i] % 2 == 0) 
+                                    pares++;
+                                else 
+                                    impares++;
+                            }
+
+                            Console.WriteLine($"Quantidade de pares: {pares}");
+                            Console.WriteLine($"Quantidade de ímpares: {impares}");
+                        }
+                        else
+                        {
+                            vetor = ArmazenarNumeros();
+                            int pares = 0;
+                            int impares = 0;
+
+                            for (int i = 0; i < vetor.Length; i++)
+                            {
+                                if (vetor[i] % 2 == 0)
+                                    pares++;
+                                else
+                                    impares++;
+                            }
+
+                            Console.WriteLine($"Quantidade de pares: {pares}");
+                            Console.WriteLine($"Quantidade de ímpares: {impares}");
+                        }
+
+                        break;
+                    default:
+                        Console.WriteLine("Por favor escolha outra opção");
+                        break;
+                }
+            }
+            while (opcao != 0);
+
+
+            static int DividirDoisNumeros(int numero1, int numero2)
+            {
+                return numero1 / numero2;
+            }
+
+            static int InformaMaiorNumero(int num1, int num2, int num3)
+            {
+                int maior = 0;
+
+                if (num1 > num2 & num1 > num3)
+                {
+                    maior = num1;
+                }else if (num2 > num1 & num2 > num3)
+                {
+                    maior = num2;
+                }
+                else if (num3 > num1 & num3 > num2)
+                {
+                    maior = num3;
+                }
+
+                return maior;
+            }
+
+
+            static int[] ArmazenarNumeros()
+            {
+                Console.WriteLine("Quantos números você deseja armazernar?");
+                int quantidadeParaArmazenar = int.Parse(Console.ReadLine());
+
+                int[] vetor = new int[quantidadeParaArmazenar];
+                for (int i = 0; i < quantidadeParaArmazenar; i++)
+                {
+                    Console.WriteLine($"Digite o {i+1}° número");
+                    vetor[i] = int.Parse(Console.ReadLine());
+                }
+                return vetor;
+            }
+
+            static int SomarNumerosArmazenados(int[] vetor)
+            {
+                int soma = 0;
+
+                for (int i = 0; i < vetor.Length; i++)
+                {
+                    soma += vetor[i];
+                }
+
+                return soma;
+            }
+        }
+    }
+}
